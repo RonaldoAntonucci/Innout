8e72750110642977e66cd1a90bf3e6e02b7ccb58
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-04 16:50:19 -0300

    login

--- public/index.php
+++ public/index.php
@@ -1,9 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-foreach (User::get([], 'name') as $user) {
-  echo $user->name;
-  echo '<br>';
-}
+require_once(CONTROLLER_PATH . '/login.php');
