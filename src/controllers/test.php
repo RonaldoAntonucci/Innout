8b70463ad07398590479a060a6d4c1d6be5b2032
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-12-09 14:15:53 -0300

    monthly report

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -1,16 +1,4 @@
 <?php
 //Controller Temporário
 
-loadModel('WorkingHours');
-
-$wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
-$workedInterval = $wh->getWorkedInterval()->format('%H:%I:%S');
-print_r($workedInterval);
-echo  '<hr>';
-
-$lunchInterval = $wh->getLunchINterval()->format('%H:%I:%S');
-print_r($lunchInterval);
-echo  '<hr>';
-
-print_r($wh->getExitTime());
-echo  '<hr>';
+print_r(getLastDayOfMonth('2019-08-04'));
